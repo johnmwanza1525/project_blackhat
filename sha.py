ef18a3c0fa64d1ad746d63d53eca78ddefd3d02a
John Mwanza <mwanza@localhost.localdomain>
2024-02-28 09:45:43 +0300

    First commit

---
+++ sha.py
@@ -0,0 +1,13 @@
+shallot=True
+while shallot:
+    def accept ():
+        Agr='Do you agree to use this tool at your own risk Y/N'
+        choice=input(agr)
+        choice=choice.upper()
+        if choice=='N':
+            print('Shallot Virus is loading')
+            shallot=False
+        else:
+            print('sorry')
+
+accept()
